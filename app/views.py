ae474906f8feb00b5c95a751251b13e7ee97e175
unknown <johnatasr@hotmail.com>
2020-08-28 20:54:23 -0300

    test final

--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 def index(request):
-    return render(request, 'templates/react/index.html', context=None)
+    return render(request, 'index.html', context=None)
--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "templates", 'react')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
