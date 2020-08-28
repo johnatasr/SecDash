7fbc632dd9c7bc66163848bba1e6cd2913232d6a
unknown <johnatasr@hotmail.com>
2020-08-28 20:41:33 -0300

    view front

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 def index(request):
-    return render(request, 'react/index.html', context=None)
+    return render(request, 'templates/react/index.html', context=None)
