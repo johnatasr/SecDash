e50f21c510b7540bd7aa2ba7eead80827672385f
unknown <johnatasr@hotmail.com>
2020-08-29 13:30:32 -0300

    add statcifiles config

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+
 
     'django_extensions',
 
